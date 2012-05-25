5d268c2725d3de61cd1f01ad4682f1dabcd98ede
Joonas Kuorilehto <joneskoo@kapsi.fi>
2012-05-26 00:44:07 +0300

    Filter host list view properly

--- hostmonitor/urls.py
+++ hostmonitor/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import ListView
-from django.db.models import Q
 
 from hostmonitor.models import Host, Network
+from hostmonitor.views import HostListView
 
-unnamed_down = Q(up=False, name=None)
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(
-        model=Network,
-    ), name='network-list'),
-    url(r'^network/(?P<slug>[^/]*)$', ListView.as_view(
-        model=Host,
-        queryset=Host.objects.order_by('-up').filter(~unnamed_down),
-    ), name='host-list'),
+    url(r'^$', ListView.as_view(model=Network), name='network-list'),
+    url(r'^network/(?P<slug>[^/]*)$', HostListView.as_view(),
+        name='host-list'),
 )
--- hostmonitor/views.py
+++ hostmonitor/views.py
@@ -1,1 +1,19 @@
 # Create your views here.
+
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
+from django.db.models import Q
+
+from hostmonitor.models import Host, Network
+
+
+class HostListView(ListView):
+    model = Host
+
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        self.network = get_object_or_404(Network, slug=slug)
+        no_unnamed_down = ~Q(up=False, name=None)
+        qs = Host.objects.order_by('-up')
+        qs = qs.filter(no_unnamed_down, network=self.network)
+        return qs
