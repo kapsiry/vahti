7e425a434823255c73b42836d9a00ba7c208bb1d
Joonas Kuorilehto <joneskoo@kapsi.fi>
2012-05-25 19:24:23 +0300

    Do not list hosts that are down and have no name

--- hostmonitor/urls.py
+++ hostmonitor/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import ListView
+from django.db.models import Q
 
 from hostmonitor.models import Host
+
+unnamed_down = Q(up=False, name=None)
 
 urlpatterns = patterns('',
     (r'^$', ListView.as_view(
         model=Host,
-        queryset=Host.objects.order_by('-up'),
+        queryset=Host.objects.order_by('-up').filter(~unnamed_down),
     )),
 )
