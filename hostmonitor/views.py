8de2e7133459353e503382462feb0c2751d108ea
Antti Jaakkola <annttu@kapsi.fi>
2012-05-26 19:29:58 +0300

    Added network name to header

--- hostmonitor/views.py
+++ hostmonitor/views.py
@@ -10,6 +10,11 @@ from hostmonitor.models import Host, Network
 class HostListView(ListView):
     model = Host
 
+    def get_context_data(self, **kwargs):
+        context = super(HostListView, self).get_context_data(**kwargs)
+        context.update({'network' : self.network})
+        return context
+
     def get_queryset(self):
         slug = self.kwargs.get('slug')
         self.network = get_object_or_404(Network, slug=slug)
