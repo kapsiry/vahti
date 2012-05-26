945cbfaf8cb2d4809c10c026722a2ac2bde37c10
Joonas Kuorilehto <joneskoo@kapsi.fi>
2012-05-26 11:10:39 +0300

    Make admin visible

--- vahti/urls.py
+++ vahti/urls.py
@@ -9,8 +9,8 @@ urlpatterns = patterns('',
     (r'vahti/', include('hostmonitor.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^vahti/admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^vahti/admin/', include(admin.site.urls)),
 )
